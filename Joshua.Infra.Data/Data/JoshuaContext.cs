109b08933f25a80f7c1da31a8e467463442eb98a
GEORGENIL ANTONIO DA SANTOS <ge.creed@gmail.com>
2024-03-23 14:43:19 -0300

    Georgenil - Novos ajustes no gitignore e configuração na camada DATA

---
+++ Joshua.Infra.Data/Data/JoshuaContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Joshua.Infra.Data.Data
+{
+    public  class JoshuaContext : IdentityDbContext
+    {
+        public JoshuaContext(DbContextOptions<JoshuaContext> options) : base(options)
+        {
+        }
+        public JoshuaContext()
+        {
+                
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                    "Data Source=DESKTOP-DIFT32I;Integrated Security=True; Initial Catalog=Joshua.MyLocal;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
